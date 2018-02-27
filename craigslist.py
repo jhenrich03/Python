bbfae8c41beb2ce227c50055edef68cae7321ba7
Josh Henrich <jhenrich003@gmail.com>
2018-02-27 13:03:31 -0600

    Update craigslist.py
    Added comments

--- craigslist.py
+++ craigslist.py
@@ -1,7 +1,9 @@
+# Import the requests and BeautifulSoup modules
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://milwaukee.craigslist.org/search/apa?hasPic=1&postedToday=1&bundleDuplicates=1&min_price=0&max_price=1500&min_bedrooms=1&availabilityMode=2&sale_date=all+dates"
+# Input a URL from the apartments/housing Craigslist page
+url = "CRAIGSLIST_URL_HERE"
 
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
@@ -14,34 +16,27 @@ Dates = []
 Addresses = []
 Links = []
 
-
+# Loop through each apartment and pull the info we want
 for a in apartments:
     descr = a.find('a', class_='result-title hdrlnk')
     link = a.find('a', class_='result-title hdrlnk').attrs['href']
     price = a.find('span', class_='result-price')
     date = a.find('time', class_='result-date')
     if not a.find('span', class_='result-hood'):
-        address = 'Milwaukee, WI'
+        address = 'Address Not Found'
     else:
         address = a.find('span', class_='result-hood').text.replace(' (', '').replace(')', '')
-
+  
     Descriptions.append(descr.text)
     Prices.append(price.text)
     Dates.append(date.text)
     Addresses.append(address)
     Links.append(link)
 
-
+# Print out the relevant info for each apartment found
 for i in range(len(apartments)):
     print("Date added: {}\n"
           "Address: {}\n"
           "Description: {}\n"
           "Price: {}\n"
           "Link: {}\n".format(Dates[i], Addresses[i], Descriptions[i], Prices[i], Links[i]))
-
-
-
-
-
-
-
